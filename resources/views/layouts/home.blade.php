d1f74c4078ee3bd6bd1164f8c5d84a083111b62b
dong <lodong601@gmail.com>
2023-09-04 20:03:41 +0700

    no message

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-    @vite(['resources/sass/shipping.scss'])
+    @vite(['/resources/sass/shipping.scss'])
   </head>
   <body>
      <div class="container-fluid">
