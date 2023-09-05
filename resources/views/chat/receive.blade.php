01f3916110a205163563cd9e845d851982efc80f
dong <lodong601@gmail.com>
2023-09-05 22:56:50 +0700

    modify

--- resources/views/chat/receive.blade.php
+++ resources/views/chat/receive.blade.php
@@ -1,7 +1,4 @@
-@extends('layouts.admin')
-@content('admin')
 <div class="left message">
   <img src="https://assets.edlin.app/images/rossedlin/03/rossedlin-03-100.jpg" alt="Avatar">
   <p>{{$message}}</p>
 </div>
-@endsection
