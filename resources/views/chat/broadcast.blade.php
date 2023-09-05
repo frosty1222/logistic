955526f79546ca269055b8fa4fe8734d12c678f2
donglv <lovandong342@gmail.com>
2023-09-05 17:29:50 +0700

    new code

---
+++ resources/views/chat/broadcast.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+@content('admin')
+<div class="right message">
+  <p>{{$message}}</p>
+  <img src="https://assets.edlin.app/images/rossedlin/03/rossedlin-03-100.jpg" alt="Profile picture">
+</div>
+@endsection
