70d956f8ccd14c4d25f50b5e13c2455b11343eeb
dong <lodong601@gmail.com>
2023-09-05 23:51:19 +0700

    add  value for seeder

--- database/seeders/UserHasRoleSeeder.php
+++ database/seeders/UserHasRoleSeeder.php
@@ -14,7 +14,11 @@ class UserHasRoleSeeder extends Seeder
     public function run(): void
     {
         $userHasRole = [
-            ['user_id'=>1,'role_id'=>1]
+            ['user_id'=>1,'role_id'=>1],
+            ['user_id'=>2,'role_id'=>2],
+            ['user_id'=>3,'role_id'=>3],
+            ['user_id'=>4,'role_id'=>5],
+            ['user_id'=>5,'role_id'=>4],
         ];
         UserHasRole::insert($userHasRole);
     }
